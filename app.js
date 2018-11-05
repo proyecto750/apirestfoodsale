3f314f3bfd95eb245e1a3bf94f772e9e47156990
Betzabe Vania Mamani Soliz <vaniamamanisoliz23@gmail.com>
2018-11-05 14:15:44 -0400

    prueba get con postman

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var  foodsale= require('./routes/apirestfoodsale');
+
+
 
 var app = express();
 
@@ -19,8 +22,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/apirestfoodsale', foodsale);
+
+
+
+
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
