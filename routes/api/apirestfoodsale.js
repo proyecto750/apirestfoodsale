d1e30870a190fd20a57e395ab5018eceba7f7a0e
Betzabe Vania Mamani Soliz <vaniamamanisoliz23@gmail.com>
2018-11-21 01:00:11 -0400

    servicios

--- routes/api/apirestfoodsale.js
+++ routes/api/apirestfoodsale.js
@@ -8,4 +8,99 @@ router.get('/', function(req, res, next) {
   });
 });
 
+router.get('/', function (req, res, next) {
+    Usuario.find().exec()
+        .then(docs => {
+            if (docs.length == 0) {
+                res.json({
+                    message: "NO hay usuarios en la BD"
+                })
+            } else {
+                res.json({
+                    count: docs.length,
+                    result: docs,
+                    request: {
+                        type: "GET"
+                    }
+
+                })
+            }
+        }).catch(err => {
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+/* save users. */
+router.post('/', function (req, res, next) {
+
+    let usuarioData = {
+        nombre: req.body.nombre,
+        ci: req.body.ci,
+        email: req.body.email,
+        password: req.body.password,
+        telefono: req.body.telefono,
+        log: req.body.log, //cadena
+        lat: req.body.lat,
+        foto: req.body.foto,
+    }
+
+    let data = new Usuario(usuarioData);
+
+    data.save()
+        .then(docs => {
+            console.log(res);
+            res.json({
+                message: "usuario guardado",
+                doc: docs
+            })
+        }).catch(err => {
+            console.log(err)
+            res.status(500).json({
+                error: err
+            });
+        });
+});
+router.patch('/:id', function (req, res, next) {
+    let idUser = req.params.id;
+    let userData = {};
+    Object.keys(req.body).forEach((key) => {
+        userData[key] = req.body[key];
+    })
+
+    Usuario.findByIdAndUpdate(idUser, userData).exec((err, result) => {
+        if (err) {
+            res.status(500).json({
+                error: err
+            });
+            return;
+        }
+        if (result) {
+            res.status(200).json({
+                message: "Se actualizaron los datos"
+
+            })
+        }
+    })
+});
+router.delete('/:id', function (req, res, next) {
+    let idUser = req.params.id;
+
+    Usuario.remove({
+        _id: idUser
+    }).exec((err, result) => {
+        if (err) {
+            res.status(500).json({
+                error: err
+            });
+            return;
+        }
+        if (result) {
+            res.status(200).json({
+                message: "Usuario eliminado",
+                result: result
+            })
+        }
+    })
+});
 module.exports = router;
