c5e791f6c8ba104523259ba1ee6740d9356f4483
Irma Veronica <verobeltrancabrera@gmail.com>
2018-11-22 17:11:56 -0400

    creado file mongoose

--- database/connect.js
+++ database/connect.js
@@ -1,11 +1,9 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/foodsale", {
-    useNewUrlParser: true
-}).then(() => {
+mongoose.connect("mongodb://localhost:27017/foodsale").then(() => {
     console.log('conexion exitosa');
 }).catch((err) => {
     console.log('error', err);
-})
+});
 
 module.exports = mongoose;
--- routes/api/apirestfoodsale.js
+++ routes/api/apirestfoodsale.js
@@ -1,12 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-
-router.get('/', function(req, res, next) {
-  res.status(200).json({
-  	msn : "hola"
-  });
-});
+const Usuario = require('../../database/models/usuario')
 
 router.get('/', function (req, res, next) {
     Usuario.find().exec()
